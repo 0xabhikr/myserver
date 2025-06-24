15331d37c197ac82048c04bc4cf89452b26930df
Abhikr <abhikr6714@gmail.com>
2025-06-24 12:30:29 +0530

    some error due in website not visiable so that why commit again

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -89,7 +89,7 @@
       <span class="blog-label" style="font-style: normal;"> 10 June , 2025 | 10:16 PM </span>
 
     </div>
-    <p style="margin-top: -1px;  font-style: normal">Think of a webhook as a way for Telegram to “ping” your server every time something
+    <p style="margin-top: -1px; font-style: normal;">Think of a webhook as a way for Telegram to “ping” your server every time something
       happens
       — like when someone sends a message to your bot. Instead of you asking Telegram every few seconds, “Hey, got
       anything
@@ -114,7 +114,7 @@
       <span class="blog-label"  style="font-style: normal;"> 10 June , 2025 | 10:16 PM </span>
 
     </div>
-    <p style="margin-top: -1px;  font-style: normal">Think of a webhook as a way for Telegram to “ping” your server every time something
+    <p style="margin-top: -1px; font-style: normal;">Think of a webhook as a way for Telegram to “ping” your server every time something
       happens
       — like when someone sends a message to your bot. Instead of you asking Telegram every few seconds, “Hey, got
       anything
@@ -136,10 +136,10 @@
           <i class="fa-solid fa-calendar-days"></i>
         </a>
       </div>
-      <span class="blog-label"  style="font-style: normal;"> 10 June , 2025 | 10:16 PM </span>
+      <span class="blog-label" style="font-style: normal;"> 10 June , 2025 | 10:16 PM </span>
 
     </div>
-    <p style="margin-top: -1px; font-style: normal";>Think of a webhook as a way for Telegram to “ping” your server every time something
+    <p style="margin-top: -1px; font-style: normal;">Think of a webhook as a way for Telegram to “ping” your server every time something
       happens
       — like when someone sends a message to your bot. Instead of you asking Telegram every few seconds, “Hey, got
       anything
